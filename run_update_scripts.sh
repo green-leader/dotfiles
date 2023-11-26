b01fa0285791c6a5d6b8f5f4c694d23d9da826ae
Sion Fandrick <1691676+green-leader@users.noreply.github.com>
2023-11-26 22:22:52 +0000

    updated scripting to use curl from wget

--- run_update_scripts.sh
+++ run_update_scripts.sh
@@ -9,7 +9,7 @@ mkdir -p $SOURCE_PATH/scripts
 manage () {
 	manage_name="$1"
 	source_url="$2"
-	wget --quiet -O "$SOURCE_PATH/scripts/$manage_name" "$source_url"
+	curl -s --output "$SOURCE_PATH/scripts/$manage_name" "$source_url"
 	# will just automatically copy file and clobber what's there
 	src="$SOURCE_PATH/scripts/$manage_name"
 	dest="$HOME/bin/$manage_name"
